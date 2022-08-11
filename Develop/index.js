6cb06a27f32f90182f400cf17ff3a26b4745e3bd
jordanrings <jordanringsakabe@gmail.com>
2022-08-11 15:41:39 -0600

    cleaned up the static information from the markdown template, reformatted the structure of the prompts and the specific prompts content to match that of the original template as well as to conform to the assignment requirements.

--- Develop/index.js
+++ Develop/index.js
@@ -1,50 +1,64 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
-const generateMarkdown = require('./utils/generateMarkdown.js'); 
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: `What is your project's name?`,
-        name: 'projectname',
+        message: `What is the title of your project?`,
+        name: 'projecttitle',
     },
     {
         type: 'input',
         message: 'Please write your name as you would like it to display in the Readme.',
-        name: 'username'
+        name: 'username',
     },
     {
         type: 'input',
         message: 'Please write a breif description of how your application functions.',
-        name: 'function'
+        name: 'description',
     },
     {
         type: 'input',
         message: 'Please describe a few problems that your application solves.',
-        name: 'solutions'
+        name: 'solutions',
     },
     {
         type: 'input',
-        message: 'Please write a few things that creating the application taught you.',
-        name: 'learn'
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation',
     },
     {
         type: 'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'usage',
+    },
+    {
+        type: 'list',
         message: 'Please write your deployed application URL.',
-        name: 'url'
+        name: 'license',
     },
-    
+    {
+        type: 'input',
+        message: 'List your collaborators, any third-party assets that require attribution, creators with links to their primary web presence, as well as tutorials with links here as well.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Write tests for your application. Then provide examples on how to run them here.',
+        name: 'tests',
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((data)=>{
+    inquirer.prompt(questions).then((data) => {
         fs.writeFileSync(`./Output/README.md`, generateMarkdown(data));
     })
 }
